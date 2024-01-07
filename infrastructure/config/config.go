3fab379126b234cc1c60d00dbf62f904a7df8ba5
MikeMwita <mwitamike8@gmail.com>
2024-01-07 22:06:44 +0300

    feat: add config loader from file or env

--- infrastructure/config/config.go
+++ infrastructure/config/config.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"errors"
+	"github.com/BurntSushi/toml"
+	"github.com/MikeMwita/go-strict/domain/datamodels"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func LoadConfig() (*datamodels.LintConfig, error) {
+	//  load the config file from the current directory
+	configPath := filepath.Join(".", "config.toml")
+	if _, err := os.Stat(configPath); err == nil {
+		var config datamodels.LintConfig
+		if _, err := toml.DecodeFile(configPath, &config); err != nil {
+			return nil, err
+		}
+		return &config, nil
+	}
+
+	//  loading the config from the environment variables
+
+	rules := os.Getenv("LINTER_RULES")
+	output := os.Getenv("LINTER_OUTPUT")
+	if rules != "" && output != "" {
+		config := datamodels.LintConfig{
+			Rules:  strings.Split(rules, ","),
+			Output: output,
+		}
+		return &config, nil
+	}
+
+	return nil, errors.New("no config file or environment variables found")
+}
