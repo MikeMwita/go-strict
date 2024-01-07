def5d88e69cef63b5e44f0b56d6cbe11576f96d1
MikeMwita <mwitamike8@gmail.com>
2024-01-07 22:13:49 +0300

    feat: add complexity service with method for calculating complexity

--- services/complexity/complexity.go
+++ services/complexity/complexity.go
@@ -1,1 +1,33 @@
 package complexity
+
+import (
+	"go/ast"
+	"go/token"
+)
+
+// Complexity is an interface that defines the complexity calculation method
+type Complexity interface {
+	Calculate(fset *token.FileSet, body *ast.BlockStmt) (int, error) // calculates the complexity of a function body
+}
+
+// ComplexityService is a service that implements the Complexity interface
+type ComplexityService struct {
+	// you can add any fields or dependencies here
+}
+
+// NewComplexityService creates a new ComplexityService
+func NewComplexityService() *ComplexityService {
+	return &ComplexityService{
+		// you can initialize any fields or dependencies here
+	}
+}
+
+// Calculate calculates the complexity of a function body
+func (cs *ComplexityService) Calculate(fset *token.FileSet, body *ast.BlockStmt) (int, error) {
+	// you can implement the complexity calculation logic here
+	// you can use the fset to get the file and line information
+	// you can use the body to traverse the abstract syntax tree of the function body
+	// you can use the rules from the Cognitive Complexity whitepaper[^1^][1] or the Gocognit project[^2^][2] as a reference
+	// you can return the complexity score as an integer and any error as an error
+	return 0, nil // this is a placeholder, you can change it as you like
+}
