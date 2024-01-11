36a9214c414a287da86efaae07f0e263d1de700e
MikeMwita <mwitamike8@gmail.com>
2024-01-11 11:18:45 +0300

    implement result model

--- domain/datamodels/result.go
+++ domain/datamodels/result.go
@@ -6,15 +6,3 @@ type Result struct {
 	Message  string
 	Severity string // "error" or "warning"
 }
-
-//type LintResult struct {
-//	// Common fields
-//	File     string `json:"file,omitempty"`
-//	Line     int    `json:"line,omitempty"`
-//	Message  string `json:"message,omitempty"`
-//	Severity string `json:"severity,omitempty"`
-//
-//	// Fields specific to function linting
-//	Function string `json:"function,omitempty"`
-//	// Add other fields as needed
-//}
