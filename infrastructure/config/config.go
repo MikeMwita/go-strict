cc1ae8d0e73ccc0f88b568b44a38ace6cbc20b55
MikeMwita <mwitamike8@gmail.com>
2024-01-11 11:19:18 +0300

    setup application config

--- infrastructure/config/config.go
+++ infrastructure/config/config.go
@@ -9,9 +9,12 @@ import (
 	"strings"
 )
 
-// LoadConfig loads the lint configuration from either a config file or environment variables
+type LinterConfig struct {
+	Rules  []string `toml:"rules"`
+	Output string   `toml:"output"`
+}
+
 func LoadConfig() (*datamodels.LintConfig, error) {
-	// load the config file from the current directory
 	configPath := filepath.Join(".", "config.toml")
 	if _, err := os.Stat(configPath); err == nil {
 		var config datamodels.LintConfig
@@ -20,7 +23,6 @@ func LoadConfig() (*datamodels.LintConfig, error) {
 		}
 		return &config, nil
 	}
-
 	// loading the config from the environment variables
 	rules := os.Getenv("LINTER_RULES")
 	output := os.Getenv("LINTER_OUTPUT")
@@ -34,30 +36,3 @@ func LoadConfig() (*datamodels.LintConfig, error) {
 
 	return nil, errors.New("no config file or environment variables found")
 }
-
-//
-//func LoadConfig() (*datamodels.LintConfig, error) {
-//	//  load the config file from the current directory
-//	configPath := filepath.Join(".", "config.toml")
-//	if _, err := os.Stat(configPath); err == nil {
-//		var config datamodels.LintConfig
-//		if _, err := toml.DecodeFile(configPath, &config); err != nil {
-//			return nil, err
-//		}
-//		return &config, nil
-//	}
-//
-//	//  loading the config from the environment variables
-//
-//	rules := os.Getenv("LINTER_RULES")
-//	output := os.Getenv("LINTER_OUTPUT")
-//	if rules != "" && output != "" {
-//		config := datamodels.LintConfig{
-//			Rules:  strings.Split(rules, ","),
-//			Output: output,
-//		}
-//		return &config, nil
-//	}
-//
-//	return nil, errors.New("no config file or environment variables found")
-//}
