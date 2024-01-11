2ecc8ddc861d1fc42cb47a6e2b682149646443b4
MikeMwita <mwitamike8@gmail.com>
2024-01-11 11:16:59 +0300

    setup complexity interfaces

--- services/complexity/complexity_interfaces.go
+++ services/complexity/complexity_interfaces.go
@@ -1,1 +1,9 @@
 package complexity
+
+//type Complexity interface {
+//	Calculate(fset *token.FileSet, body *ast.BlockStmt) (int, error) // calculates the complexity of a function body
+//	If(stmt *ast.IfStmt) int                                         // calculates complexity for if statements
+//	Loop(stmt ast.Node) int                                          // calculates complexity for loops
+//	Switch(stmt ast.Node) int                                        // calculates complexity for switch statements
+//	Case(stmt *ast.CaseClause) int                                   // calculates complexity for case clauses
+//}
