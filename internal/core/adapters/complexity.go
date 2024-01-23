ddf0ad9afd8d163ccd240a1ae5febcb5c4a9f348
MikeMwita <mwitamike8@gmail.com>
2024-01-23 15:01:50 +0300

    update complexity adapter

--- internal/core/adapters/complexity.go
+++ internal/core/adapters/complexity.go
@@ -1,1 +1,19 @@
 package adapters
+
+import (
+	"go/ast"
+	"go/token"
+)
+
+type Complexity interface {
+	Calculate(fset *token.FileSet, node ast.Node) (int, error)
+	//GetDetail(result *models.LintResult) string
+}
+
+type ComplexityCalculator interface {
+	Calculate(fset *token.FileSet, body *ast.BlockStmt) (int, error)
+	If(stmt *ast.IfStmt) int
+	Loop(stmt ast.Node) int
+	Switch(stmt ast.Node) int
+	Case(stmt *ast.CaseClause) int
+}
