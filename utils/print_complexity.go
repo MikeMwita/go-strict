7e50dbcb3a06eca3d69806194a45cb216af5e4e1
MikeMwita <mwitamike8@gmail.com>
2024-01-24 22:44:46 +0300

    Decouple printcomplexity funtion

--- utils/print_complexity.go
+++ utils/print_complexity.go
@@ -42,7 +42,6 @@ func PrintComplexity(results []*models.LintResult, format string, detailsFormat 
 		fmt.Println("No functions were found to calculate the average complexity per function")
 	} else {
 		avgComplexity := float64(totalComplexity) / float64(funcCount)
-
 		fmt.Printf("%.2f = overall average complexity per function\n", avgComplexity)
 	}
 
@@ -56,29 +55,35 @@ func PrintComplexity(results []*models.LintResult, format string, detailsFormat 
 
 	for _, result := range results {
 		if result.File != "" && result.Function != "" {
-			if format == "line-number" {
-				fmt.Printf("%s:%d:1 - %s has complexity: %s\n", result.File, result.Line, result.Function, result.Message)
-			} else if format == "json" {
-				// Add JSON format printing logic here
-			} else {
-				// Default format if none is specified
-				fmt.Printf("%s:%d:1 - %s has complexity: %s\n", result.File, result.Line, result.Function, result.Message)
-			}
-			// Check if the detailsFormat is true
-			if detailsFormat {
-				// Get the complexity details for the function
-				details := complexity.GetDetail(result)
-
-				// Print the complexity details in a code block
-				fmt.Println("```go")
-				fmt.Println(details)
-				fmt.Println("```")
-			}
+			printResultDetails(result, format, detailsFormat)
 		}
 	}
 }
 
-//func PrintComplexity(results []*models.LintResult, format string) {
+func printResultDetails(result *models.LintResult, format string, detailsFormat bool) {
+	if format == "line-number" {
+		fmt.Printf("%s:%d:1 - %s has complexity: %s\n", result.File, result.Line, result.Function, result.Message)
+	} else if format == "json" {
+		// Add JSON format printing logic here
+	} else {
+		// Default format if none is specified
+		fmt.Printf("%s:%d:1 - %s has complexity: %s\n", result.File, result.Line, result.Function, result.Message)
+	}
+
+	// Check if the detailsFormat is true
+	if detailsFormat {
+		// Get the complexity details for the function
+		details := complexity.GetDetail(result)
+
+		// Print the complexity details in a code block
+		fmt.Println("```go")
+		fmt.Println(details)
+		fmt.Println("```")
+	}
+}
+
+//
+//func PrintComplexity(results []*models.LintResult, format string, detailsFormat bool) {
 //	var fileCount, funcCount, totalComplexity, maxComplexity, complexLineCount int
 //
 //	for _, result := range results {
@@ -116,17 +121,31 @@ func PrintComplexity(results []*models.LintResult, format string, detailsFormat 
 //
 //	fmt.Printf("%d = complex lines\n\n", complexLineCount)
 //
+//	// Check if the format is "complexity"
+//	if format == "complexity" {
+//		// Change the format to "line-number"
+//		format = "line-number"
+//	}
+//
 //	for _, result := range results {
 //		if result.File != "" && result.Function != "" {
 //			if format == "line-number" {
 //				fmt.Printf("%s:%d:1 - %s has complexity: %s\n", result.File, result.Line, result.Function, result.Message)
 //			} else if format == "json" {
 //				// Add JSON format printing logic here
-//			} else if format == "complexity" {
-//				// Add complexity format printing logic here
 //			} else {
 //				// Default format if none is specified
 //				fmt.Printf("%s:%d:1 - %s has complexity: %s\n", result.File, result.Line, result.Function, result.Message)
+//			}
+//			// Check if the detailsFormat is true
+//			if detailsFormat {
+//				// Get the complexity details for the function
+//				details := complexity.GetDetail(result)
+//
+//				// Print the complexity details in a code block
+//				fmt.Println("```go")
+//				fmt.Println(details)
+//				fmt.Println("```")
 //			}
 //		}
 //	}
